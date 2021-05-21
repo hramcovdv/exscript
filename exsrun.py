e43c92ed7429d7f1e5681479985e0a9b7eaa1e00
Dmitriy Kharmtsov <hramcovdv@gmail.com>
2021-05-21 19:29:54 +0300

    1.6

--- exsrun.py
+++ exsrun.py
@@ -1,114 +1,156 @@
-#!/usr/bin/env python3
-"""
-Exscript runner tool.
+"""Exscript runner tool.
 
 Usage:
-  exsrun encode <text>
-  exsrun [options] (-s --script=FILE)... <host>...
-  exsrun [options] (-f --fromfile=FILE) (-s --script=FILE)...
+  exsrun cipher WORD
+  exsrun [options] [-v | -vv | -vvv] (--script=FILE)... (--hosts=FILE | HOST)...
   exsrun -h | --help
   exsrun --version
 
 Options:
   -h --help                 Show this screen.
   --version                 Show version.
-  -c NUM --connections=NUM  Set maximum connections [default: 1].
-  -v NUM --verbose=NUM      Set out debug information [default: 1].
-  -a FILE --accounts=FILE   Get username/password from file [default: acc.cfg].
-  -l FILE --logging=FILE    Save logging information to file.
-  -p PASS --password=PASS   Set authorization password.
-  -f FILE --fromfile=FILE   Get hosts from file.
+  -f FILE --hosts=FILE      Get hosts from file.
   -s FILE --script=FILE     Add the script file to the queue.
+  -u USER --username=USER   Set authentication username.
+  -p PSWD --password=PSWD   Set authentication password.
+  -a FILE --accounts=FILE   Get username/password from file.
+  -l FILE --logging=FILE    Save logging information to file.
+  -c NUM --connections=NUM  Set maximum connections [default: 1].
   --status                  Print status information in end.
-  --summarize               Print summarize information in end.
-"""
+  --summarize               Print summarize information in end."""
 import re
 import os
-import logging
+import sys
 import base64
+import logging
+from getpass import getpass
 from docopt import docopt
-from Exscript import Queue, Logger
+from Exscript import Queue, Logger, Account, Host
 from Exscript.util.log import log_to
+from Exscript.util.decorator import bind
 from Exscript.util.report import status, summarize
 from Exscript.util.file import get_accounts_from_file, get_hosts_from_file
+from Exscript.parselib.exception import ExecuteError
 import Exscript.util.template as template
-
-args = docopt(__doc__, version="1.4.8")
-
-if args["encode"]:
-    print(base64.b64encode(bytes(args["<text>"], "utf-8")).decode("utf-8"))
-    exit(0)
-
-check_exists = []
-
-check_exists.append(args["--accounts"])
-check_exists.append(args["--fromfile"])
-check_exists.extend(args["--script"])
-
-for filename in list(filter(None, check_exists)):
-    if not os.path.exists(filename):
-        exit("Specified file '%s' does not exist." % filename)
-
-hosts = args["<host>"] or get_hosts_from_file(args["--fromfile"])
-
-accounts = get_accounts_from_file(args["--accounts"])
-
-for account in accounts:
-    account.set_authorization_password(args["--password"])
-
-script = ""
-
-for filename in args["--script"]:
-    script += ''.join(filter(str.strip, open(filename).readlines()))
-
-template.test(script)
-
-username_prompt = re.compile(r"(user ?name|user ?id|login)[\w\s()<>]*: *$",
-                             re.IGNORECASE)
-password_prompt = re.compile(r"(password)[\w\s()<>]*: *$",
-                             re.IGNORECASE)
-
-login_error_prompt = re.compile(r"""[\r\n]?[^\r\n]*(?:bad secrets|bad password|
-                                denied|invalid|too short|incorrect|
-                                connection timed out|rejected|
-                                fail|failed|failure)""",
-                                re.IGNORECASE | re.X)
-
-error_prompt = re.compile(r"^%?\s*(?:error|invalid|incomplete|\
-                          unrecognized|unknown command|failure|failed|fail|\
-                          connection timed out|[^\r\n]+ not found)",
-                          re.IGNORECASE)
 
 logger = Logger()
 
-@log_to(logger)
+LOGIN_ERROR_PROMPT = re.compile(
+    r"[\r\n]?[^\r\n]*(?:bad secrets|bad password|denied|invalid|too short|incorrect|connection timed out|rejected|fail|failed|failure)",
+    re.IGNORECASE)
 
-def do_job(job, host, conn):
-    conn.set_login_error_prompt(login_error_prompt)
-    conn.set_username_prompt(username_prompt)
-    conn.set_password_prompt(password_prompt)
-    conn.set_error_prompt(error_prompt)
-    conn.app_authenticate()
+ERROR_PROMPT = re.compile(
+    r"^%?\s*(?:error|invalid|incomplete|unrecognized|unknown command|failure|failed|fail|connection timed out|[^\r\n]+ not found)",
+    re.IGNORECASE)
+
+USERNAME_PROMPT = re.compile(
+    r"(user ?name|user ?id|login)[\w\s()<>]*: *$",
+    re.IGNORECASE)
+
+PASSWORD_PROMPT = re.compile(
+    r"(password)[\w\s()<>]*: *$",
+    re.IGNORECASE)
+
+
+def read_file(filename):
+    """ Read text file
+    """
+    with open(filename) as file:
+        return file.read()
+
+
+def cipher(word, encoding='utf-8'):
+    """ Base64 encrypted word
+    """
+    hashed = base64.b64encode(word.encode(encoding=encoding))
+    return hashed.decode(encoding=encoding)
+
+
+def init_connection(func):
+    """ Initializing the connection
+    """
+    def wrapper(job, host, conn, script):
+        conn.set_login_error_prompt(LOGIN_ERROR_PROMPT)
+        conn.set_error_prompt(ERROR_PROMPT)
+        conn.set_username_prompt(USERNAME_PROMPT)
+        conn.set_password_prompt(PASSWORD_PROMPT)
+        conn.app_authenticate()
+        func(job, host, conn, script)
+
+    return wrapper
+
+
+@log_to(logger)
+@init_connection
+def do_job(job, host, conn, script):
+    """ Run script on host if authenticated
+    """
     if conn.is_app_authenticated():
         template.eval(conn, script)
 
+
 def job_cb(jobname, exc_info):
-    logging.basicConfig(stream=open(args["--logging"] or os.devnull, 'a'),
-                        format='%(asctime)s %(message)s')
-    logging.warning("%s %s: %s", jobname,
-                    exc_info[1].__class__.__name__,
-                    ('%r' % str(exc_info[1]))[1:-1])
+    """ Callback as log report
+    """
+    class_name = exc_info[1].__class__.__name__
+    message = ('%r' % str(exc_info[1]))[1:-1]
 
-queue = Queue(verbose=int(args["--verbose"]),
-              max_threads=int(args["--connections"]),
-              exc_cb=job_cb,
-              stderr=open(os.devnull, 'w'))
+    logging.warning("%s %s: %s", jobname, class_name, message)
 
-queue.add_account(accounts)
-queue.run(hosts, do_job)
-queue.shutdown()
 
-if args["--status"]:
-    print(status(logger))
-if args["--summarize"]:
-    print(summarize(logger))
+if __name__ == "__main__":
+    args = docopt(__doc__, version="1.6")
+
+    logging.basicConfig(filename=args['--logging'],
+                        datefmt="%Y-%m-%d %H:%M:%S",
+                        level=logging.WARNING,
+                        format="%(asctime)s %(levelname)s %(message)s")
+
+    if args['cipher']:
+        print(cipher(args['WORD']))
+        sys.exit(0)
+
+    check_files = args['--hosts'] + args['--script'] + [args['--accounts']]
+    for filename in filter(None, check_files):
+        if not os.path.isfile(filename):
+            sys.exit('No such file: %s' % filename)
+
+    hosts = []
+    if args['--hosts']:
+        for filename in args['--hosts']:
+            hosts += get_hosts_from_file(filename)
+
+    if args['HOST']:
+        hosts += [Host(host) for host in args['HOST']]
+
+    all_in_one_script = ""
+    for filename in args['--script']:
+        all_in_one_script += read_file(filename)
+
+    try:
+        template.test(all_in_one_script)
+    except ExecuteError as error:
+        sys.exit(error)
+
+    queue = Queue(
+        verbose=int(args['-v']),
+        max_threads=int(args['--connections']),
+        # stdout=open(os.devnull, "w"),
+        stderr=open(os.devnull, "w"),
+        exc_cb=job_cb)
+
+    if args['--accounts']:
+        queue.add_account(get_accounts_from_file(args['--accounts']))
+    else:
+        queue.add_account(
+            Account(args['--username'] or input('Enter username: '),
+                    args['--password'] or getpass('Enter password: ')))
+
+    queue.run(hosts, bind(do_job, all_in_one_script))
+    queue.shutdown()
+
+    if args['--status']:
+        print(status(logger))
+
+    if args['--summarize']:
+        print(summarize(logger))
